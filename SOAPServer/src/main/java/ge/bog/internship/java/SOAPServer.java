e1be07e8496f59ea9edb0f1809add9cc92c65324
vakhokoto <v.kotoreishvili@gmail.com>
2020-06-05 15:17:54 +0400

    log4j

--- SOAPServer/src/main/java/ge/bog/internship/java/SOAPServer.java
+++ SOAPServer/src/main/java/ge/bog/internship/java/SOAPServer.java
@@ -1,11 +1,14 @@
 package ge.bog.internship.java;
 
+import org.apache.log4j.Logger;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import java.util.List;
 
 @WebService
 public class SOAPServer {
+    private static final Logger logger = Logger.getLogger(SOAPServer.class.getName());
 
     @WebMethod
     public String classifyUserData(List<Object[]> resultList){
